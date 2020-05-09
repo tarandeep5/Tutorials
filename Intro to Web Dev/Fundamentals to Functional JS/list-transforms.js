46fae47bccad6e12f86f7c467d405dd6a1ac577a
Tarandeep <tmittal@ryerson.ca>
2020-05-09 00:01:20 -0400

    Update list-transforms.js

--- Intro to Web Dev/Fundamentals to Functional JS/list-transforms.js
+++ Intro to Web Dev/Fundamentals to Functional JS/list-transforms.js
@@ -16,4 +16,31 @@ function loopSuspects () {
     }
 }
 
+ var gameLoop = function () {
+     for (var i = 0; i.game.suspects.length; i++) {
+         for (var j in game.suspects[i]) {
+             if (game.suspects[i][j] === "Rusty"){
+                 console.log("Found em!");
+             }
+             else {
+                 console.log("Next time!");
+             }
+         }
+     }
+ }
+
+
+ function destructure () {
+     colourArray = []
+     for (var i in game.suspects){
+        colourArray.push = game.suspects[i].colour
+        console.log(game.suspects[i].colour)
+     }
+     return colourArray;
+ }
+ 
 loopSuspects();
+colourArr = destructure();
+let [firstColour, secondColour] = [colourArr [0], colourArr[1]];
+console.log(firstColour);
+
